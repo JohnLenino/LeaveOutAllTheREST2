78a90ba1d677a23d659c870d8c480313dab69dd6
JohnLenino <konstantinbykov3122008@gmail.com>
2024-03-07 21:57:53 +0700

    second_task

--- app/data/jobs_api.py
+++ app/data/jobs_api.py
@@ -8,10 +8,19 @@ from .jobs import Jobs
 blueprint = flask.Blueprint('jobs_api', __name__, template_folder='templates')
 
 @blueprint.route('/api/jobs')
-def get_news():
+def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     print(jobs)
     print(type(jobs))
     return jsonify(
         {'jobs': [item.to_dict(only=('title', 'wealth')) for item in jobs]})
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
+def get_one_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'jobs': jobs.to_dict(only=('id', 'title'))})
